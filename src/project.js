0234d671a23dbc8436841e155d968b507d94ef1d
Johan Svensson <johan.svensson0608@gmail.com>
2025-10-08 15:19:48 +0200

    ändrat style flexboxgrid

--- src/project.js
+++ src/project.js
@@ -1,23 +1,24 @@
 export default class Projects extends HTMLElement {
-  constructor(){
+  constructor() {
     super();
     this.projects = [];
-    console.log("hej")
+    console.log("hej");
   }
-  async connectedCallback(){
+  async connectedCallback() {
     this.projects = await this.getProjects();
     this.render();
   }
-  async getProjects(){
-    const response = await fetch('./src/projects.json')
+  async getProjects() {
+    const response = await fetch("./src/projects.json");
     const result = await response.json();
     return result;
   }
-  render(){
+  render() {
     this.innerHTML = `
-      <h2>Mina Projekt</h2>
       <div class="project-container">
-        ${this.projects.map(project => `
+        ${this.projects
+          .map(
+            (project) => `
           <div class="project-card">
             <h3>${project.name}</h3>
             <img src="${project.image}" alt="${project.link}" class="project-image" />
@@ -26,18 +27,18 @@ export default class Projects extends HTMLElement {
               <a href="#" class="project-link" data-link="${project.link}">Gå till projekt</a>
             </div>
           </div>
-        `).join("")}
+        `
+          )
+          .join("")}
       </div>
     `;
 
-    this.querySelectorAll('.project-link').forEach(link => {
-      link.addEventListener('click', (e) => {
+    this.querySelectorAll(".project-link").forEach((link) => {
+      link.addEventListener("click", (e) => {
         e.preventDefault();
         const url = link.dataset.link;
         window.open(url);
       });
     });
-
   }
-
 }
