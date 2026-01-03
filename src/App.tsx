3e389578ab75e4fcdbec52dcd146f5719d54d280
Johan Svensson <johan.svensson0608@gmail.com>
2026-01-03 16:40:46 +0100

    gitignore föll bort, browserroute

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/contexts/ThemeContext";
-import { LanguageProvider } from "@/contexts/LanguageContext";
+import { LanguageProvider } from "@/contexts/LanguageContext.";
 import Index from "./pages/Index";
 import ProjectPage from "./pages/ProjectPage";
 import NotFound from "./pages/NotFound";
@@ -18,11 +18,10 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/portfolio">
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/project/:projectSlug" element={<ProjectPage />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
