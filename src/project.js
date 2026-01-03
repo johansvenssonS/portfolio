8965c6446ef182e663844ceb60d0f32917416965
Johan Svensson <johan.svensson0608@gmail.com>
2026-01-03 15:57:30 +0100

    fix med style,react osv

---
+++ src/project.js
@@ -0,0 +1,44 @@
+export default class Projects extends HTMLElement {
+  constructor() {
+    super();
+    this.projects = [];
+    console.log("hej");
+  }
+  async connectedCallback() {
+    this.projects = await this.getProjects();
+    this.render();
+  }
+  async getProjects() {
+    const response = await fetch("./src/projects.json");
+    const result = await response.json();
+    return result;
+  }
+  render() {
+    this.innerHTML = `
+      <div class="project-container">
+        ${this.projects
+          .map(
+            (project) => `
+          <div class="project-card">
+            <h3>${project.name}</h3>
+            <img src="${project.image}" alt="${project.link}" class="project-image" />
+            <div class="project-info">
+              <p>${project.description}</p>
+              <a href="#" class="project-link" data-link="${project.link}">Gå till projekt</a>
+            </div>
+          </div>
+        `
+          )
+          .join("")}
+      </div>
+    `;
+
+    this.querySelectorAll(".project-link").forEach((link) => {
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const url = link.dataset.link;
+        window.open(url);
+      });
+    });
+  }
+}
